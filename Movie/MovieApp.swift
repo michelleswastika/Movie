a5855832d33efa31c49e2acfdf6d10093a06a674
michelleswastika <mswastika@student.ciputra.ac.id>
2023-04-27 18:38:26 +0700

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Michelle Swastika on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
